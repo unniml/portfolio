493d5fe855b2700a4aec6bb865a89e0c9e98bcaf
M L Unnikrishnan Lenin <unniml@yahoo.com>
2026-07-27 09:26:39 +0530

    Made changes to layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,25 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 export const metadata: Metadata = {
   title: "Unnikrishnan | Personal Portfolio",
   description:
-    "Unnikrishnan is a full stack wordpress developer with 8 years experience.",
+    "Unnikrishnan is a full stack wordpress developer with 25+ years experience.",
 };
 
 export default function RootLayout({
@@ -19,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className="antialiased bg-gray-50 text-gray-950 relative pt-28 sm:pt-36"
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] -z-10 rounded-full filter blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] -z-10 rounded-full filter blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
